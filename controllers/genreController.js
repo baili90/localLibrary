343ecebd552937ff0c9b91c223cdf0e96fc8b145
朱玉青 <qianshibuwang@yahoo.com>
2020-04-24 18:58:14 +0800

    交互

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,6 +2,8 @@ var Book = require('../models/book');
 var async = require('async');
 
 const Genre = require('../models/genre');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // 显示完整的藏书种类列表
 exports.genre_list = (req, res) => {
@@ -41,12 +43,58 @@ exports.genre_detail = (req, res) => {
 
 // 由 GET 显示创建藏书种类的表单
 exports.genre_create_get = (req, res) => {
-  res.send('未实现：藏书种类创建表单的 GET');
+  res.render('genre_form', { title: 'Create Genre' });
 };
 
 // 由 POST 处理藏书种类创建操作
 exports.genre_create_post = (req, res) => {
-  res.send('未实现：创建藏书种类的 POST');
+  // Validate that the name field is not empty.
+  body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+    
+  // Sanitize (trim and escape) the name field.
+  sanitizeBody('name').trim().escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+
+      // Create a genre object with escaped and trimmed data.
+      var genre = new Genre(
+        { name: req.body.name }
+      );
+
+
+      if (!errors.isEmpty()) {
+          // There are errors. Render the form again with sanitized values/error messages.
+          res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+      return;
+      }
+      else {
+          // Data from form is valid.
+          // Check if Genre with same name already exists.
+          Genre.findOne({ 'name': req.body.name })
+              .exec( function(err, found_genre) {
+                   if (err) { return next(err); }
+
+                   if (found_genre) {
+                       // Genre exists, redirect to its detail page.
+                       res.redirect(found_genre.url);
+                   }
+                   else {
+
+                       genre.save(function (err) {
+                         if (err) { return next(err); }
+                         // Genre saved. Redirect to genre detail page.
+                         res.redirect(genre.url);
+                       });
+
+                   }
+
+               });
+      }
+  }
 };
 
 // 由 GET 显示删除藏书种类的表单
